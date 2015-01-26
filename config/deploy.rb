3b10a8c621d816c7000e871e6b91f6ea9b16b302
ShaoweiBai <michael.shwbai@gmail.com>
2015-01-26 22:15:23 +0800

    [FIX] deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, "megavera"
-set :repo_url, "git://github.com/bsw85/shiyeitaly.git"
+set :repo_url, "git://github.com/bsw85/megavera.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
